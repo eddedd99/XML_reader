4a77c837597d2caecf0e9922cb23a19953a9f3be
Edd <edcruces99@gmail.com>
2021-07-13 07:52:18 -0500

    XML Reader versión 1.0
    XML Reader versión 1.0 programado en vb.net C Sharp

---
+++ XML_reader/XML_reader/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace XML_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(110, 87);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(169, 100);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Leer XML";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 375);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+    }
+}
+
---
+++ XML_reader/XML_reader/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;   //Agregar
+
+namespace XML_reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            //using (XmlReader reader = XmlReader.Create(@"D:\Users\eduardo.cruces\Documents\Visual Studio 2013\Projects\SIVAC\SIVAC\bin\Debug\SIVAC.xml"))
+            using (XmlReader reader = XmlReader.Create(@"SIVAC.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        //return only when you have START tag  
+                        switch (reader.Name.ToString())
+                        {
+                            case "Ruta":
+                                MessageBox.Show("Ruta es : " + reader.ReadString());
+                                break;
+                            case "Base":
+                                MessageBox.Show("Base de datos es : " + reader.ReadString());
+                                break;
+                        }
+                    }                    
+                }
+            }
+            //MessageBox.Show("Finalizo");
+        }
+    }
+}
